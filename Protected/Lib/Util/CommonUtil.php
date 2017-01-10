70d22f7c9e6bbe397cbadf49385c5e5daa744179
fp-jidan <394777474@qq.com>
2017-01-10 12:28:48 +0800

    update

--- Protected/Lib/Util/CommonUtil.php
+++ Protected/Lib/Util/CommonUtil.php
@@ -91,4 +91,40 @@ class CommonUtil {
                 return var_export($var, true);
         }
     }
+    
+    /**
+     * 数字自动三位三位加逗号
+     * @param $num
+     * @return array|string
+     */
+    public static function tran($num)
+    {
+        $v = explode('.',$num);//把整数和小数分开
+        $rl = $v[1];//小数部分的值
+        $j = strlen($v[0]) % 3;//整数有多少位
+        $sl = substr($v[0], 0, $j);//前面不满三位的数取出来
+        $sr = substr($v[0], $j);//后面的满三位的数取出来
+        $i = 0;
+        $rvalue='';
+        while( $i <= strlen($sr) ){
+            $rvalue = $rvalue.','.substr($sr, $i, 3);//三位三位取出再合并，按逗号隔开
+            $i = $i + 3;
+        }
+        $rvalue = $sl.$rvalue;
+        $rvalue = substr($rvalue,0,strlen($rvalue)-1);//去掉最后一个逗号
+        $rvalue = explode(',',$rvalue);//分解成数组
+        if($rvalue[0]==0){
+            array_shift($rvalue);//如果第一个元素为0，删除第一个元素
+        }
+        $rv = $rvalue[0];//前面不满三位的数
+        for($i = 1; $i < count($rvalue); $i++){
+            $rv = $rv.','.$rvalue[$i];
+        }
+        if(!empty ($rl)){
+            $rvalue = $rv.'.'.$rl;//小数不为空，整数和小数合并
+        }else{
+            $rvalue = $rv;//小数为空，只有整数
+        }
+        return $rvalue;
+    }
 }
